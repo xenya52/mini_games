e29ff61e8d449b1469995e21c6684cf9d9c5b77a
xenya52 <108221381+xenya52@users.noreply.github.com>
2024-05-06 19:58:01 +0200

    first commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# P = pawn
+# T = Rook
+# K = Knights
+# B = Bishop
+# Q = Queen
+# W = King
+
+rows, cols = (8, 8)
+board = [["." for i in range(cols)] for j in range(rows)]
+
+# while True:
+for i in board:
+  for j in board:
+    print(j)
+  print()
